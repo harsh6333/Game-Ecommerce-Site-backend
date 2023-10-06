dff5471356dc4db93a81b036579a461b66739111
harsh6333 <theoneharsh2003@gmail.com>
2023-10-06 22:28:08 +0530

    seventh commit

--- app.js
+++ app.js
@@ -4,15 +4,18 @@ import Createuser from "./Routes/CreateUser.js";
 import Games from "./models/games.js";
 import cors from "cors";
 import "dotenv/config";
+
 const app = express();
-const PORT =3000;
+const PORT = 3000;
+
 app.use(
   cors({
-    origin: "http://localhost:5173", // Allow requests from your frontend's origin
-    methods: "GET,HEAD,PUT,PATCH,POST,DELETE", // Allow the HTTP methods you need
-    allowedHeaders: ["Content-Type", "Authorization"], // Allow the "Authorization" header
+    origin: "http://localhost:5173",
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
+
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "http://localhost:5173");
   res.header(
@@ -21,18 +24,47 @@ app.use((req, res, next) => {
   );
   next();
 });
-mongoose.connect("mongodb://localhost:27017/Gamesite", {
-  serverSelectionTimeoutMS: 30000,
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
-global.games = await Games.find({}).maxTimeMS(30000);
-// console.log(global.games);
+
+global.games = []; // Initialize the global.games variable
+
+async function connectToDatabase() {
+  try {
+    await mongoose.connect("mongodb://127.0.0.1:27017/Gamesite", {
+      serverSelectionTimeoutMS: 30000,
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
+    console.log("Connected to MongoDB");
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error);
+    process.exit(1);
+  }
+}
+
+async function fetchGamesFromDatabase() {
+  try {
+    global.games = await Games.find({});
+    console.log("Fetched games from MongoDB:", global.games);
+  } catch (error) {
+    console.error("Error fetching games from MongoDB:", error);
+  }
+}
+
 app.get("/", (req, res) => {
-  res.send("hello");
+  res.send("Hello");
 });
+
 app.use(express.json());
 app.use("/api", Createuser);
-app.listen(PORT, () => {
-  console.log(`server running on ${PORT}`);
-});
+
+connectToDatabase()
+  .then(fetchGamesFromDatabase)
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.error("Error starting the server:", error);
+  });
