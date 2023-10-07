9dbf48bf71fe5bf5917d40d6833816688972bf33
harsh6333 <theoneharsh2003@gmail.com>
2023-10-07 08:27:14 +0530

    seventh commit

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ import Createuser from "./Routes/CreateUser.js";
 import Games from "./models/games.js";
 import cors from "cors";
 import "dotenv/config";
-
 const app = express();
 const PORT = 3000;
 
@@ -29,11 +28,9 @@ global.games = []; // Initialize the global.games variable
 
 async function connectToDatabase() {
   try {
-    await mongoose.connect("mongodb://127.0.0.1:27017/Gamesite", {
-      serverSelectionTimeoutMS: 30000,
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(
+      `mongodb+srv://${process.env.USER}:${process.env.PASSWORD}@cluster0.xrynsi5.mongodb.net/Gamesite`
+    );
 
     console.log("Connected to MongoDB");
   } catch (error) {
@@ -44,16 +41,12 @@ async function connectToDatabase() {
 
 async function fetchGamesFromDatabase() {
   try {
-    global.games = await Games.find({}).maxTimeMS(30000); 
+    global.games = await Games.find({}).maxTimeMS(30000);
     console.log("Fetched games from MongoDB");
   } catch (error) {
     console.error("Error fetching games from MongoDB:", error);
   }
 }
-
-app.get("/", (req, res) => {
-  res.send("Hello");
-});
 
 app.use(express.json());
 app.use("/api", Createuser);
